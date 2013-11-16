73c05cb1a243ea3fef6d9956cbf32446f18e4918
srgg6701 <srgg6701@gmail.com>
2013-11-16 23:30:49 +0400

    Доработка блоков с адвокатами.

--- js/debug.js
+++ js/debug.js
@@ -24,13 +24,20 @@ $(function(){
 	if(bg=urlParams.debug){ 
 		/* 	значения для bg:
 			1 - подложка для верхней части страницы
-			2 - для нижней
+			2 - для средней 
+			3 - для нижней
 		 */
 		//document.write('>');
 		$('#css_screen').after('<link media="screen" href="stylesheets/debug.css" rel="stylesheet" type="text/css">');
 		$('#bg_container').css('background','none');
+		
+		var bgPos=(bg==3)?
+			'70px -2px': '172px 0';
+		
 		$('body').css({
-			background:'url(_sources/pg_test'+bg+'.png) 172px 0 no-repeat fixed'
+			background:'url(_sources/pg_test'+bg+'.png)  no-repeat fixed',
+			backgroundPosition:bgPos,
+			backgroundSize:'initial'
 		});	
 		$('#content').css('opacity',0.5);
 		$('#top').css('opacity',0.5);
